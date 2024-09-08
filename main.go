9b449bd2759e6bfb7b99c484f2795f4c27ece413
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-09-09 00:54:43 +0900

    Goの基本的な出力

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main //Goはpackageの宣言から
 //fmtはformatパッケージ
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
 //簡単な出力　　main関数がエントリーポイントでプログラムの本体
 func main(){
 	fmt.Println("Goの練習")
+	fmt.Println(time.Now())
 }
-//Goの練習と出力
+/*
+Goの練習と出力
+今の日付が出力
+*/
